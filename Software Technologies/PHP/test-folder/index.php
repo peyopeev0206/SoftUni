dce902d1068aa079816a2dbba630be98acfe1c46
Peyo Peev <ppeev@mi9retail.com>
2018-03-28 22:37:42 +0300

    Adding configuration in Dockerfile for mySQL

--- Software Technologies/PHP/test-folder/index.php
+++ Software Technologies/PHP/test-folder/index.php
@@ -1,9 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: peyo
- * Date: 3/28/18
- * Time: 10:55 AM
- */
 
-echo "Check if it's workig";
+$numberOne = 1;
+$numberTwo = 2;
+
+$sumOfTwoNumbers = $numberOne + $numberTwo;
+
+echo ($sumOfTwoNumbers === 3) ? '1 plus 2 equals 3!' : 'Something is terribly wrong';
+echo '<br>';
+$servername = "docker-mysql";
+$username = "root";
+$password = "docker";
+$database = "blog";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
